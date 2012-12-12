73c5e6cfadcbaf5417c651552a5eb6f779c21868
adjohnston <adam.johnston3679@gmail.com>
2012-12-12 15:31:37 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NeilMaul::Application.config.secret_token = 'abd110702d7af8525e419d8ac56ee6d2c130043e6c5928ff84f85703828998123a124c3ab723807f609726700d7af1208f7636faa8e55a4bc9ddef6c0871962b'
