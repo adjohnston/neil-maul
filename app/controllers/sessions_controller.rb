c0fe8e0a898c6f68ded1933e8c6dd4b71f1cee47
adjohnston <adam.johnston3679@gmail.com>
2012-12-13 10:41:16 +0000

    setup login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
+    session[:redirect_page] = request.env['HTTP_REFERER'].nil? ? :root : request.env['HTTP_REFERER']
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to session[:redirect_page], notice: "Welcome back #{user.name}"
+    else
+      redirect_to login_path, alert: "Oops! There was an error, please check your email address and password"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    session[:redirect_page] = request.env['HTTP_REFERER'].nil? ? :root : request.env['HTTP_REFERER']
+    redirect_to session[:redirect_page]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 NeilMaul::Application.routes.draw do
-  get "sessions/new"
+  
+  resources :sessions
+
+  match 'login' => 'sessions#new', as: 'login'
+  match 'logout' => 'sessions#destroy', as: 'logout'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +54,8 @@ NeilMaul::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
