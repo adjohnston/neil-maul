de16eaace5e6c70b776697f3b3e9f863cd186412
adjohnston <adam.johnston3679@gmail.com>
2012-12-14 10:49:33 +0000

    updated style for mobile, tablet and desktop, minor tweaks to coding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private 
+
+  def authenticate
+    if !auth_user
+      redirect_to login_path, alert: 'To see this page, you need to be logged in'
+    end
+  end
+
+  def auth_user
+    @user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :authenticate, :auth_user
 end
